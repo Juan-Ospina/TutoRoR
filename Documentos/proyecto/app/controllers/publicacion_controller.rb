faae207adf6d83d80247dbb50e279cd3ef47552b
Juan Ospina <juanospinasuarez@gmail.com>
2015-02-26 19:18:48 -0500

    Primera Versión

---
+++ Documentos/proyecto/app/controllers/publicacion_controller.rb
@@ -0,0 +1,15 @@
+class PublicacionController < ApplicationController
+	def index
+		@publicaciones=Publicacion.all
+	end
+
+	def new
+		@publicacion=Publicacion.new
+	end
+
+	def create
+		@publicacion=Publicacion.new(params[:publicacion].permit(:titulo, :contenido))
+		@publicacion.save
+		redirect_to publicacion_path	#Se explica la ruta...
+	end
+end
